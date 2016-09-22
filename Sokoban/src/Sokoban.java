d4fd2588c8227ed1177280bce396d43abba7a451
Claire Krawiec <c.m.krawiec12@gmail.com>
2016-09-22 08:57:24 -0600

    made a board to print out, and made instructions

--- Sokoban/src/Sokoban.java
+++ Sokoban/src/Sokoban.java
@@ -1,8 +1,39 @@
 
-public class Sokoban {
-
-	public static void main(String[] args) {
-		//new SokobanFrame(400, 800);
+public class Sokoban
+{
+	
+	static String myArray[][] = new String[25][25];
+	public static void main(String[] args) 
+	{
+		for(int row = 0; row < myArray.length; row++)
+			{
+				for(int col = 0; col < myArray.length; col++)
+					{
+						myArray[row][col]="+";
+						System.out.println(myArray[row][col]);
+					}
+			}
+		System.out.println("The # is your charcater, the + is your boxes, and the * is where you are supposed to place your box.");
+		System.out.println();
+		System.out.println("------------------------------------------");
+		System.out.println(" #                                       |");
+		System.out.println("                                         |");
+		System.out.println("|          +                             |");
+		System.out.println("|                                        |");
+		System.out.println("|                                        |");
+		System.out.println("|            *                           |");
+		System.out.println("|                        +               |");
+		System.out.println("|                                        |");
+		System.out.println("|                                        |");
+		System.out.println("|                                *       |");
+		System.out.println("|        +                               |");
+		System.out.println("|                                        |");
+		System.out.println("|                 *                      |");
+		System.out.println("|                                        |");
+		System.out.println("|                                        |");
+		System.out.println("| *                         +            |");
+		System.out.println("------------------------------------------");
+		
+		
 	}
-
 }
